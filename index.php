5ee373bb58cab82f063751d95488269bf3686355
Rohmat Setyono <tiowayahu@gmail.com>
2022-10-24 11:01:39 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Tio";
+$umur = 21;
+
+echo "Nama saya <b>$nama</b>, saya berusia $umur tahun.";
+
+$namaAdik = "jhon";
+$umurAdik = 12;
+
+echo "<br>Nama adik saya <b>$namaAdik</b>, adik saya berusia $umurAdik tahun.";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "<br>Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
