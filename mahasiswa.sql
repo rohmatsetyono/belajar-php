da9efd27b8326d84d6a5613e825a4437d9d65045
Rohmat Setyono <tiowayahu@gmail.com>
2022-11-10 11:08:20 +0700

    add form handling

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -1,11 +1,14 @@
+-- buat database fakultas
 CREATE DATABASE fakultas;
 
+-- buat database jurusan
 CREATE TABLE jurusan(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     kode CHAR(4) NOT NULL,
     nama VARCHAR(50) NOT NULL
 );
 
+-- buat database mahasiswa
 CREATE TABLE mahasiswa(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_jurusan INTEGER NOT NULL,
@@ -17,3 +20,15 @@ CREATE TABLE mahasiswa(
     alamat VARCHAR(225) NOT NULL,
     FOREIGN KEY (id_jurusan) REFERENCES jurusan(id)
 );
+
+-- insert jurusan
+insert into jurusan (kode, nama) values ("PTIF", "Pendidikan Teknik Informatika");
+
+-- insert mahasiswa
+insert into mahasiswa (id_jurusan, nim, nama, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat) values (1, "20220001", "Mahmud", "laki-laki", "Jakarta", "2002-02-18", "Jl.jalanin aja 8");
+
+-- delete
+delete from mahasiswa where id = 2;
+
+-- update mahasiswa
+update mahasiswa set nim = "20220002" where id = 2;
